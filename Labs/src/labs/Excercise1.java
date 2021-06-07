6c7a616b4a8995b14ab1445a2b736d7e0ab1c54c
Prajwal <Prajwal@Azzuri>
2021-06-07 20:23:43 +0530

    First Excersice in Lab1

---
+++ Labs/src/labs/Excercise1.java
@@ -0,0 +1,27 @@
+package labs;
+
+import java.util.Scanner;
+
+public class Excercise1 {
+	public static void main(String[] args) {
+		
+		////Accept User Input in variable num
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please Enter an interger - ");
+		int num = sc.nextInt();
+		
+		///declare variables rem and sum
+		int rem,sum=0;
+		
+		///Loop over a number
+		while(num>0) {
+			rem = num%10;
+			sum= sum+(rem*rem*rem);
+			num= num/10;
+		}
+		
+		//Print Sum of cubes of digit in a number
+		System.out.println(sum);
+		sc.close();
+	}
+}
